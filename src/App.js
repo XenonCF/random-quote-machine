a982d24fe75c454e04ad1f0ca9aaf6f5246e9f4b
XenonCF <cuello.fernando12@gmail.com>
2023-08-10 19:51:59 -0300

    Finnished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSquareTwitter, faSquareTumblr } from '@fortawesome/free-brands-svg-icons'
+import { faQuoteLeft } from '@fortawesome/free-solid-svg-icons'
+import { useState, useEffect } from "react";
 
-function App() {
+
+export default function App() {
+  const [state, setState] = useState([{}])
+  const [currentAuthor, setCurrentAuthor] = useState("")
+  const [currentQuote, setCurrentQuote] = useState("")
+  const [currentIndex, setCurrentIndex] = useState(0)
+  
+  useEffect(() => {
+    const dataFetch = async () => {
+      const data = await (
+        await fetch('https://api.quotable.io/quotes/random?limit=50', {
+          method: 'GET',
+          contentType: 'application/json'
+        })
+      ).json();
+      
+      setState(
+        data.map(result => ({quote: result.content, author: result.author}))
+      );
+      
+    };
+
+    dataFetch();
+  }, []);
+
+  useEffect(() => setCurrentAuthor(state[currentIndex].author), [state, currentIndex]);
+  useEffect(() => setCurrentQuote(state[currentIndex].quote), [state, currentIndex]);
+
+  function randomIndex() {
+    setCurrentIndex(Math.floor(Math.random() * 50))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+      <div className="card" style={{width: 700}}>
+        <div className="card-body">
+          <blockquote id='quote-box' className="blockquote mb-0 ">
+            <p id="text"><FontAwesomeIcon icon={faQuoteLeft}/>{currentQuote}</p>
+            <footer id='author' className="blockquote-footer"><cite title="Source Title">{currentAuthor}</cite></footer>
+            <a id='tweet-quote' href={'https://twitter.com/intent/tweet?text="' + currentQuote + '" -' + currentAuthor} className="card-link"><FontAwesomeIcon icon={faSquareTwitter} /></a>
+            <a id='tumblr' href={"https://www.tumblr.com/widgets/share/tool?posttype=quote&caption="+currentAuthor+"&content="+currentQuote+"&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button"}className="card-link"><FontAwesomeIcon icon={faSquareTumblr} /></a>
+            <div id="spacer"></div>
+            <button id="new-quote" className="card-link" onLoad={randomIndex} onClick={randomIndex}>New Quote</button>
+          </blockquote>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
